6c35b1909f30e8bca435525a03779bc1d788aa2d
Jake <jacobartz1987@gmail.com>
2019-07-11 17:19:42 -0600

    arguments, optional random or specified address.

--- mac_manipulator.py
+++ mac_manipulator.py
@@ -3,37 +3,55 @@
 # Media Access Control (MAC) address changer.
 # This program prompts the user for a new MAC address and what interface to assign it to.
 # This helps maintain anonymity and allows impersonation.
+# Input interface type using -i or --interface.
+# Input a mac address or leave input blank to generate random.
 
 # subprocess is a module that allows CLI interaction from the script.
 
 import subprocess
 import optparse
 from random import seed
-# from random import randint
+from random import randint
 
 seed
 
 parser = optparse.OptionParser()
 
-parser.add_option("-i", "--interface", dest="interface", help="Choose an interface, view options with ifconfig.")
-parser.add_option("-m", "--mac", dest="new_MAC_address",
-                  help="Choose an address 'aa:bb:cc:dd:ee:ff'.")
+parser.add_option("-i", "--interface",
+                  dest="interface",
+                  help="Choose an interface, view options with ifconfig.")
+parser.add_option("-m", "--mac",
+                  dest="new_MAC_address",
+                  help="Choose an address 'aa:bb:cc:dd:ee:ff', or leave argument out to generate random.")
 
 (options, arguments) = parser.parse_args()
 
-# mac_array = ["10"]  # MAC addresses must start with an even number.
-#
-# for _ in range(5):
-#     mac_element = str(randint(10, 99))
-#     mac_array.append(mac_element)
+
+def generate_mac_address():
+    for _ in range(5):
+        mac_element = str(randint(10, 99))
+        mac_array.append(mac_element)
 
 
-interface = options.interface
-new_MAC_address = options.new_MAC_address
-# new_MAC_address = ":".join(mac_array)
+if options.interface:
+    interface = options.interface
+else:
+    print("[readme] You may supply arguments when calling the script, see help. "
+          "Or enter the interface you wish to use now and a random address will be generated.")
+    interface = input("[i] choose an interface >")
 
-print("[+] Changing MAC address for " + interface + " to " + new_MAC_address)
+if options.new_MAC_address:
+    new_MAC_address = options.new_MAC_address
+    print("[!] input address used")
+else:
+    mac_array = ["00"]  # MAC addresses must start with an even number.
+    generate_mac_address()
+    new_MAC_address = ":".join(mac_array)
+    print("[!] random MAC address generated")
+
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_MAC_address])
 subprocess.call(["ifconfig", interface, "up"])
+
+print("[+] changing MAC address for " + interface + " to " + new_MAC_address)
